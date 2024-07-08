6acba6c76943ec79dc209fa76346d05c513588fe
Borislav2-0 <crva20@mail.ru>
2024-07-08 22:19:12 +0300

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -1,33 +1,51 @@
 import time
 
 
+class Video:
+    def __init__(self, title: str, duration: int, adult_mode: bool = False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+    def __eq__(self, other):
+        return self.title == other.title
+
+    def __contains__(self, item):
+        return item in self.title
+
+
 class UrTube:
-    users = []
-    videos = []
-    current_user = None
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
 
     def log_in(self, login: str, password: str):
         for user in self.users:
             if login == user.nickname and password == user.password:
                 self.current_user = user
+            break
 
     def register(self, nickname: str, password: str, age: int):
+        password = hash(password)
         for user in self.users:
-            if nickname in user.nickname:
+            if user.nickname == nickname:
                 print(f"Пользователь {nickname} уже существует")
-                break
-        else:
-            user = User(nickname, password, age)
-            self.users.append(user)
-            self.log_out()
-            self.log_in(user.nickname, user.password)
+                return
+
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        self.current_user = new_user
+        print(f"Пользователь {nickname} зарегистрирован и вошел в систему.")
 
     def log_out(self):
         self.current_user = None
 
     def add(self, *args):
         for movie in args:
-            self.videos.append(movie)
+            if movie not in self.videos:
+                self.videos.append(movie)
 
     def get_videos(self, text: str):
         movie_list = []
@@ -42,7 +60,7 @@ class UrTube:
         elif self.current_user:
             for video in self.videos:
                 if movie in video.title:
-                    for i in range(1, 11):
+                    for i in range(1, video.duration):
                         print(i, end=' ')
                         time.sleep(1)
                     print('Конец видео')
@@ -51,17 +69,6 @@ class UrTube:
 
     def __str__(self):
         return f"{self.videos}"
-
-
-class Video:
-    def __init__(self, title: str, duration: int, time_now: int = 0, adult_mode: bool = False):
-        self.title = title
-        self.duration = duration
-        self.time_now = time_now
-        self.adult_mode = adult_mode
-
-    def __str__(self):
-        return f"{self.title}"
 
 
 class User:
@@ -94,6 +101,7 @@ ur.register('vasya_pupkin', 'lolkekcheburek', 13)
 ur.watch_video('Для чего девушкам парень программист?')
 ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
 ur.watch_video('Для чего девушкам парень программист?')
+ur.watch_video('Лучший язык программирования 2024 года')
 
 ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
 print(ur.current_user)
